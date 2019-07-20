9fe63ad86fd56dfdf41d3dd5948965da8778c639
jesse <jesse.katsumata@cureapp.jp>
2019-07-20 19:12:12 +0900

    feat: add meta component

---
+++ head/Meta.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+
+const Meta = () => (
+  <Head>
+    <title>Naturalclar</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta name="twitter:description" content={"Naturalclar's portfolio site"} />
+    <meta property="og:description" content={"Naturalclar's portfolio site"} />
+    <meta property="og:locale" content={"en_US"} />
+    <meta property="og:title" content={"Naturalclar"} />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content={`https://naturalclar.dev/`} />
+    <meta
+      property="og:image"
+      content={`https://naturalclar.dev/static/cat_square.jpg`}
+    />
+    <meta
+      property="article:published_time"
+      content={new Date("2019/07/20").toISOString()}
+    />
+    <meta property="article:author" content="naturalclar" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="NaturalClar" />
+    <meta
+      name="twitter:image"
+      content={`https://naturalclar.dev/static/cat_square.jpg`}
+    />
+    <link
+      rel="alternate"
+      type="application/json+oembed"
+      href={`https://naturalclar.dev/`}
+      title={"Naturalclar"}
+    />
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossOrigin="anonymous"
+    />
+  </Head>
+);
+
+export default Meta;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,31 @@
 import { NextPage } from "next";
 import { useAmp } from "next/amp";
 import { Avatar } from "../components/atoms";
-
+import { Meta } from "../head";
 export const config = { amp: "hybrid" };
 
 const Index: NextPage<{}> = () => {
   const isAmp = useAmp();
   return (
     <>
-      <Avatar src="/static/cat_square.jpg" isAmp={isAmp} />
-      <p>
-        Welcome to the {isAmp ? "AMP" : "normal"} version of the Index page!!
-      </p>
+      <Meta />
+      <section
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <Avatar src="/static/cat_square.jpg" isAmp={isAmp} />
+        <h2>Naturalclar</h2>
+        <p>Front-end Developer at CureApp, Inc.</p>
+        <p>Node.js, TypeScript, ReactNative</p>
+        <div style={{ display: "flex" }}>
+          <i className="fa fa-twitter fa-lg" />
+          <i className="fa fa-github fa-lg" />
+        </div>
+      </section>
     </>
   );
 };
