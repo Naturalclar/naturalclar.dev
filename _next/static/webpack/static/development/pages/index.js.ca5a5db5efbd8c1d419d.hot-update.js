8d37df546d2667a168e05df087a1737b8fc31ae7
jesse <jesse.katsumata@cureapp.jp>
2019-07-20 15:13:19 +0900

    Updates

---
+++ _next/static/webpack/static/development/pages/index.js.ca5a5db5efbd8c1d419d.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/atoms/Avatar.tsx":
+/*!*************************************!*\
+  !*** ./components/atoms/Avatar.tsx ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/jesse/.ghq/github.com/Naturalclar/naturalclar.dev/components/atoms/Avatar.tsx";
+
+
+var Avatar = function Avatar(_ref) {
+  var src = _ref.src;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("amp-img", {
+    style: styles.container,
+    src: src,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  });
+};
+
+var styles = {
+  container: {
+    width: 24,
+    height: 24,
+    borderRadius: 12
+  }
+};
+/* harmony default export */ __webpack_exports__["default"] = (Avatar);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.ca5a5db5efbd8c1d419d.hot-update.js.map
