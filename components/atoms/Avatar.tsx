a52a92141cf41bdb353fc72aa6ceb32304ac467a
Naturalclar <jesse.katsumata@gmail.com>
2019-07-21 11:52:17 +0900

    chore: add alt attribute

--- components/atoms/Avatar.tsx
+++ components/atoms/Avatar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 type Props = {
+  alt?: string;
   src: string;
   size?: number;
   isAmp?: boolean;
 };
 
-const Avatar = ({ src, size = 100, isAmp = false }: Props) => {
+const Avatar = ({ alt = "", src, size = 100, isAmp = false }: Props) => {
   const style = { width: size, height: size, borderRadius: size / 2 };
   return isAmp ? (
-    <amp-img style={style} src={src} width={size} height={size} />
+    <amp-img style={style} src={src} width={size} height={size} alt={alt} />
   ) : (
-    <img style={style} src={src} />
+    <img style={style} src={src} alt={alt} />
   );
 };
 export default Avatar;
