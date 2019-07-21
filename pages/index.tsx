83733cb3ba20531a90e682bfc06912271fa9425c
Naturalclar <jesse.katsumata@gmail.com>
2019-07-21 11:52:31 +0900

    feat: update content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,33 @@
 import { NextPage } from "next";
 import { useAmp } from "next/amp";
-import { Avatar } from "../components/atoms";
-import { Meta } from "../head";
+import { Avatar, Paragraph, Heading } from "../components/atoms";
+import { Page } from "../components/templates";
+
 export const config = { amp: "hybrid" };
 
 const Index: NextPage<{}> = () => {
   const isAmp = useAmp();
   return (
-    <>
-      <Meta />
+    <Page>
       <section
         style={{
           display: "flex",
           justifyContent: "center",
           flexDirection: "column",
-          alignItems: "center"
+          alignItems: "center",
+          padding: 24
         }}
       >
-        <Avatar src="/static/cat_square.jpg" isAmp={isAmp} />
-        <h2>Naturalclar</h2>
-        <p>Front-end Developer at CureApp, Inc.</p>
-        <p>Node.js, TypeScript, ReactNative</p>
+        <Avatar src="/static/cat_square.jpg" isAmp={isAmp} alt="MyIcon" />
+        <Heading>Naturalclar</Heading>
+        <Paragraph>Front-end Developer at CureApp, Inc.</Paragraph>
+        <Paragraph>Node.js, TypeScript, React, React-Native</Paragraph>
         <div style={{ display: "flex" }}>
           <i className="fa fa-twitter fa-lg" />
           <i className="fa fa-github fa-lg" />
         </div>
       </section>
-    </>
+    </Page>
   );
 };
 
