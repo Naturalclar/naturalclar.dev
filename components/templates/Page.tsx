50bbed5145024f49c3531ad3f44d55451071a81f
Naturalclar <jesse.katsumata@gmail.com>
2019-07-21 11:51:36 +0900

    feat: add page component

---
+++ components/templates/Page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const style = {
+  container: {
+    position: "absolute" as "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    fontFamily: "Source Sans Pro"
+  }
+};
+
+type Props = {
+  color?: string;
+};
+
+const Page: React.FC<Props> = ({ children, color = "aliceblue" }) => (
+  <main style={{ ...style.container, backgroundColor: color }}>{children}</main>
+);
+
+export default Page;
