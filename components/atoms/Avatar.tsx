5413e5bb9f805010936a881174b8254185039861
Jesse Katsumata <jesse.katsumata@gmail.com>
2025-06-19 21:54:58 +0900

    chore: update to next 15 (#37)

--- components/atoms/Avatar.tsx
+++ components/atoms/Avatar.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Avatar = ({ alt = "", src, size = 60, isAmp = false }: Props) => {
   const style = { width: size, height: size, borderRadius: size / 2, opacity: 0.5 };
   return isAmp ? (
-    <amp-img style={style} src={src} width={size} height={size} alt={alt} />
+    React.createElement('amp-img', { style, src, width: size, height: size, alt } as any)
   ) : (
     <img style={style} src={src} alt={alt} />
   );
